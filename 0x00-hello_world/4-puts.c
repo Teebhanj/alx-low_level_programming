8aa6c96ee955e8830cabff2f129c1d5c01f4e1a6
Teebhanj <oyebanjioluwatosin1@gmail.com>
2022-12-08 18:29:05 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
-/*Hello World!*/
 #include <stdio.h>
+
+/*
+ *main- prints "\"Programming is like building a multilingual puzzle"
+ *return- 0 if exited properly, else non-zero
+ */
 int main(void)
 {	puts("\"Programming is like building a multilingual puzzle"); 
-		return 0;
+		return (0);
 }
