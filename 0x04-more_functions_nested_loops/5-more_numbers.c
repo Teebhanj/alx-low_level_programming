41975f7dda2d24e9d2a7277f94a5ef30f7e410aa
Teebhanj <oyebanjioluwatosin1@gmail.com>
2022-12-16 19:12:27 -0800

    more numbers 1

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 time numbers 0 to 14
+ * followed by new line
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		_putchar(i + '0');
+		{
+			int j
+
+			while (j < 11)
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
