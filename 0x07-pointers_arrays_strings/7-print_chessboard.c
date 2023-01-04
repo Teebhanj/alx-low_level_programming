2ef1e75246050f9c72bf17da6e415f8583a0cefb
Teebhanj <oyebanjioluwatosin1@gmail.com>
2023-01-04 07:00:19 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+/**
+ * print_chessboard - prints chessboard
+ *
+ * @a: 2d array of chars
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8]);
+{
+	/*variables*/
+	int n, j;
+
+	n = 0;
+	while (n < 8) /*repeat number*/
+	{
+		j = 0;
+
+		while (j < 8)
+		{
+			_putchar (a[n][j]);
+			j++;
+		}
+		_putchar ('n'); /*prints new line*/
+		n++;
+	}
+}
