42fc1c635adf20de60621d943e537180124786dd
Teebhanj <oyebanjioluwatosin1@gmail.com>
2023-01-09 12:21:32 -0800

    what is my name?

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /*
- * main - prints the name of program
- * Return: return 0
+ * main - prints the name of program, followed by a new line.
+ * @argc: argument count.
+ * @argc: array that contian the program line argument.
+ * Return: return 0.
  */
 
 int main (int argc, char *argv[])
 {
-	printf("my name is %s/n", argv[0])
+	printf("my name is %s\n", argv[0])
 		return 0;
 }
