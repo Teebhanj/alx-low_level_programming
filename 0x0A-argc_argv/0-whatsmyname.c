e8f8907bbb83fc40fce76c63df0bfa3a1f66e1ea
Teebhanj <oyebanjioluwatosin1@gmail.com>
2023-01-09 12:03:16 -0800

    what is my name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - prints the name of program
+ * Return: return 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("my name is %s/n", argv[0])
+		return 0;
+}
