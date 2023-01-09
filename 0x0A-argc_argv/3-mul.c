0f2cb6455b5a298736167ca58bb75ae2ac50aa58
Teebhanj <oyebanjioluwatosin1@gmail.com>
2023-01-09 14:24:17 -0800

    multiplication of argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: number of argument in acommand line.
+ * @argv: array of arguments contained in a command line.
+ * Return: 0 is success 1 is error.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n")
+			return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
